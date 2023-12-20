93d4e7c9616e65d577a49e95228b12def92d3c4c
Neeraj319 <itskop520@gmail.com>
2023-12-20 18:39:06 +0545

    feat: add url path params

--- main.go
+++ main.go
@@ -18,10 +18,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := CreateRouter()
-	r.addRoute("/", index)
+	r.addRoute("/home", index)
+	r.addRoute("/home/:somePath", index)
+	r.addRoute("/file/:fileName/:somethingElse", fileHandler)
+	r.addRoute("/file/:fileName/geda/:somethingElse", fileHandler)
+	
 	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		fmt.Printf("faile to start server: %s \n", err)
-	}
 
+	if err != nil {
+		fmt.Printf("failed to start server: %s \n", err)
+	}
 }
--- router.go
+++ router.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	// "fmt"
+	"fmt"
 	"net/http"
 	"reflect"
 	"runtime"
@@ -16,14 +18,38 @@ func default404Response(w http.ResponseWriter, req *http.Request) {
 type SimpleRouter struct {
 	routeMapping map[string]HandlerFunction
 	Response404  HandlerFunction
+	pathParams   map[string][]int
 }
 
 func (r *SimpleRouter) addRoute(path string, function HandlerFunction) {
+	if strings.Contains(path, ":") {
+		pathParams := strings.Split(path, ":")
+		if len(pathParams) == 2 {
+			params := make([]int, 1)
+			params[0] = 1
+			r.pathParams[pathParams[0]] = params
+		} else {
+			var params []int
+			var paramsPathName string
+			for i, value := range path {
+				if rune(value) == ':' {
+					params = append(params, i-1)
+				}
+			}
+			for _, value := range strings.Split(path, "/") {
+				if (value) != "" && value[0] != ':' {
+					paramsPathName += value
+				}
+			}
+			r.pathParams[paramsPathName] = params
+			fmt.Println(r.pathParams)
+		}
+	}
 	r.routeMapping[path] = function
 }
 
 func CreateRouter() *SimpleRouter {
-	return &SimpleRouter{routeMapping: make(map[string]HandlerFunction), Response404: default404Response}
+	return &SimpleRouter{routeMapping: make(map[string]HandlerFunction), Response404: default404Response, pathParams: make(map[string][]int)}
 }
 
 func GetFunctionName(temp interface{}) string {
@@ -33,6 +59,7 @@ func GetFunctionName(temp interface{}) string {
 
 func (r *SimpleRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	url := req.URL.String()
+	fmt.Println(url)
 	function := r.routeMapping[url]
 
 	if function == nil && r.Response404 != nil {
