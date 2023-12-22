a2717e0b21a29215538555275a6e075f4a12f91e
Neeraj319 <itskop520@gmail.com>
2023-12-22 12:16:29 +0545

    fix: reponse values

--- main.go
+++ main.go
@@ -5,34 +5,53 @@ import (
 	"net/http"
 )
 
-func fileHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "file handler no random")
+func fileNotRandom(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "/file/:fileName/:anotherParam")
 }
 
-func fileHandler2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "file handler with random")
+func fileRandom(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "/file/:fileName/random/:somethingElse")
 }
+
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is index")
+	fmt.Fprintf(w, "/")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is home ")
+	fmt.Fprintf(w, "/home")
 }
+
+func homeSomePath2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "/:somePath/asdf/:nothing")
+}
+
 func homeSomePath(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is with some path ")
+	fmt.Fprintf(w, "/home/:somePath")
 }
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello ")
+	fmt.Fprintf(w, "/:anyParam")
 }
+
+func random(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "/random")
+}
+
+func randomWithParams(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "/random/:params")
+}
+
 func main() {
 	r := CreateRouter()
 	r.addRoute("/home", home)
 	r.addRoute("/", index)
-	r.addRoute("/:hello", hello)
+	r.addRoute("/:anyParam", hello)
 	r.addRoute("/home/:somePath", homeSomePath)
-	r.addRoute("/file/:fileName/:somethingElse", fileHandler)
-	r.addRoute("/file/:fileName/random/:somethingElse", fileHandler2)
+	r.addRoute("/random", random)
+	r.addRoute("/random/:params", randomWithParams)
+	r.addRoute("/:somePath/asdf/:nothing", homeSomePath2)
+	r.addRoute("/file/:fileName/:anotherParam", fileNotRandom)
+	r.addRoute("/file/:fileName/random/:somethingElse", fileRandom)
 
 	err := http.ListenAndServe(":8080", r)
 
