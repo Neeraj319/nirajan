63968ddaab138c5bad87ccfdfbc50bc53e01283b
Neeraj319 <itskop520@gmail.com>
2023-12-30 14:47:40 +0545

    fix: check for query params

--- router.go
+++ router.go
@@ -313,11 +313,7 @@ func callHandler(handler interface{}, pathArray []string, handerObj *RouteHandle
 }
 
 func (r *SimpleRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	url := req.URL.String()
-	i := strings.IndexRune(url, '?')
-	if i != -1 {
-		url = url[:i]
-	}
+	url := req.URL.Path
 	pathArray := strings.Split(url, "/")
 	pathArray = removeBlankStrings(pathArray)
 
